a63b776a70daced3045d94a9b3e900a9b76e7793
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-02-26 15:43:34 +0700

    ref(auth_request_dto): make dto support simple validation

--- src/main/java/com/wiormiw/digilott_20/v1/domain/dto/AuthRequestDTO.java
+++ src/main/java/com/wiormiw/digilott_20/v1/domain/dto/AuthRequestDTO.java
@@ -1,6 +1,8 @@
 package com.wiormiw.digilott_20.v1.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AuthRequestDTO(
-   String username,
-   String password
+        @NotBlank String username,
+        @NotBlank String password
 ) {}
