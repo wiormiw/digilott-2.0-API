35cef1f834119080293731c5812cf2f4ae98afe9
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-02-26 15:40:53 +0700

    feat(roles): Add custom roles fungsionality

--- src/main/java/com/wiormiw/digilott_20/v1/infrastructure/repository/UserRepository.java
+++ src/main/java/com/wiormiw/digilott_20/v1/infrastructure/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
 }
