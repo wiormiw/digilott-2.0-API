6dd150dbe901c45f4c9302b404154421d0bc7108
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-03-05 01:28:16 +0700

    style(resource_not_found_exception): Remove http status annotation

--- src/main/java/com/wiormiw/digilott_20/v1/domain/exception/ResourceNotFoundException.java
+++ src/main/java/com/wiormiw/digilott_20/v1/domain/exception/ResourceNotFoundException.java
@@ -3,7 +3,6 @@ package com.wiormiw.digilott_20.v1.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
