e3582907bb881c79f8258567a77aa53b6aa9c99e
Wijoyo Raharjo Murti Indra Wahyono <wwijoyo2@gmail.com>
2025-03-05 02:18:07 +0700

    ref(room_repository): Add custom query to get all the room that tied with users (participants)

--- src/main/java/com/wiormiw/digilott_20/v1/infrastructure/repository/RoomRepository.java
+++ src/main/java/com/wiormiw/digilott_20/v1/infrastructure/repository/RoomRepository.java
@@ -2,8 +2,13 @@ package com.wiormiw.digilott_20.v1.infrastructure.repository;
 
 import com.wiormiw.digilott_20.v1.domain.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface RoomRepository extends JpaRepository<Room, UUID> {
+    @Query("SELECT r FROM Room r JOIN r.participants p WHERE p.username = :username")
+    Set<Room> findRoomsByParticipantUsername(@Param("username") String username);
 }
